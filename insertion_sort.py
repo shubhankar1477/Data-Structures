0bc0bb33148fe20f658f6b3e11254fe99d73b000
Shubhankar Deskpande <shubhankardeshpande.14@gmail.com>
2023-02-15 12:21:04 -0500

    Create insertion_sort.py
    Basics of Data structures

---
+++ insertion_sort.py
@@ -0,0 +1,12 @@
+def insertion_sort(array):
+    for i in range(1,len(array)):
+        key_item = array[i]
+        j = i-1
+        while j >=0 and array[j] < key_item:
+            array[j + 1] = array[j]
+            j -= 1
+        array[j + 1] = key_item
+    return array
+        
+if __name__ == '__main__':
+    print(insertion_sort([8, 2, 6, 4, 5]))
